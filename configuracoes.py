7bbfca2a310d319dbe6aadd5a807aa099f91a567
Guilherme Lucas <guiilucasdev@gmail.com>
2026-01-24 17:21:12 -0300

    Exclusão da lista global de palavras.

--- configuracoes.py
+++ configuracoes.py
@@ -17,16 +17,3 @@ LETRAS = ["A", "Á", "Â", "Ã", "À", "B", "C", "D", "E", "É",
           "Ê", "F", "G", "H", "I", "Í", "J", "K", "L", "M",
           "N", "O", "Ó", "Ô", "Õ", "P", "Q", "R", "S", "T",
           "U", "Ú", "V", "W", "X", "Y", "Z"]
-
-PALAVRAS = {"PLACA", "FONTE", "MEMÓRIA", "PROCESSADOR",
-            "DISCO", "TECLADO", "MOUSE", "MONITOR",
-            "GABINETE", "COOLER", "BARRAMENTO", "SLOT",
-            "VÁLVULA", "ENIAC", "TRANSISTOR", "UNIX",
-            "MACINTOSH", "MAINFRAME", "ASSEMBLY",
-            "MICROPROCESSADOR", "APPLE", "FORTRAN",
-            "WINDOWS", "GUI", "TERMINAL", "EMBARCADO",
-            "FIRMWARE", "INTERRUPÇÃO", "THREAD", "DRIVER",
-            "PROCESSO", "BUFFER", "BOOT", "KERNEL", 
-            "BIOS", "ESCALONADOR", "PYTHON", "JAVA",
-            "LUA", "TYPESCRIPT", "COBOL", "PHP",
-            "SWIFT", "KOTLIN", "RUST", "RUBY"}
