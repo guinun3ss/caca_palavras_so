74aa742f98fe858662a5eb6cd2da0ce227715698
Guilherme Lucas <guiilucasdev@gmail.com>
2026-01-25 12:25:48 -0300

    Remoção de variáveis desnecessárias e atualização de comentários.

--- configuracoes.py
+++ configuracoes.py
@@ -1,8 +1,9 @@
-TAMANHO_MATRIZ = 25
-TAMANHO_CELULA = 25
+TAMANHO_MATRIZ = 25 # define quantas letras a matriz vai ter
+TAMANHO_CELULA = 25 # tamanho de cada célula em px
 
 MARGEM = 20
 
+# tamanho final da janela que o jogo vai ocupar
 LARGURA_JANELA = TAMANHO_MATRIZ * TAMANHO_CELULA + MARGEM * 2
 ALTURA_JANELA = TAMANHO_MATRIZ * TAMANHO_CELULA + MARGEM * 2
 
@@ -10,9 +11,10 @@ COR_FUNDO = "white"
 COR_CONTORNO = "gray"
 COR_TEXTO = "black"
 
-COR_SELECAO = "#f5dd9d"
-COR_ACHADA = "#c0d88c"
+COR_SELECAO = "#f5dd9d" # cor enquanto o mouse é pressionado
+COR_ACHADA = "#c0d88c" # cor persistente se a palavra for correta
 
+# as letras daqui são sorteadas para formar a matriz sem palavras
 LETRAS = ["A", "Á", "Â", "Ã", "À", "B", "C", "D", "E", "É",
           "Ê", "F", "G", "H", "I", "Í", "J", "K", "L", "M",
           "N", "O", "Ó", "Ô", "Õ", "P", "Q", "R", "S", "T",
